363e182f899e84887ee39793b1b450ffdf322b2f
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-01-13 04:21:19 +0100

    feat: added page changing

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -30,14 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    // Note: Don't forget to use set State
-    setState(() {
-      _counter++;
-    });
-  }
+  int _activePage = 0;
 
   // Called when setState is called
   @override
@@ -52,59 +45,54 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: <Widget>[
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Home page',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-              tooltip: 'Feeds',
-              icon: const Icon(Icons.home),
-              onPressed: () {},
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Account',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.inventory)
-            ),
-            IconButton(
-              tooltip: 'Account',
-              icon: const Icon(Icons.person),
-              onPressed: () {},
-            ),
-          ],
+          ),
         ),
+      ][_activePage],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            _activePage = index;
+          });
+        },
+        selectedIndex: _activePage,
+        destinations: const <Widget>[
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home'
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+              icon: Icon(Icons.person_outline),
+              label: 'Account'
+          )
+        ]
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
