1b317e01a006d778eb641fbc953e9435dce62c9f
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-01-13 21:23:51 +0100

    fix: changed wrong name import

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wisefood/main.dart';
+import 'package:tooGoodToWaste/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
