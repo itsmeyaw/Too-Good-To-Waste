0d1a19db397752d15bdbf1fce8361f10f1c2e419
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-02-06 19:42:34 +0100

    fix: Fixed user location service only returning value once

--- frontend/lib/service/user_location_service.dart
+++ frontend/lib/service/user_location_service.dart
@@ -1,38 +1,44 @@
 import 'package:location/location.dart';
 import 'package:logger/logger.dart';
+import 'package:synchronized/synchronized.dart';
 
 final Logger logger = Logger();
 
 class UserLocationService {
   static final Location loc = Location();
+  static Lock lock = Lock();
 
   static Future<LocationData> getUserLocation() async {
-    bool serviceEnabled = await loc.serviceEnabled();
+    return lock.synchronized(() async {
+      bool serviceEnabled = await loc.serviceEnabled();
+      logger.d('Start querying user location');
 
-    if (!serviceEnabled) {
-      logger.d('Location service is not enabled, requesting service');
-      loc.requestService();
-    }
-    if (!serviceEnabled) {
-      throw Exception("Cannot enabled location service");
-    }
+      if (!serviceEnabled) {
+        logger.d('Location service is not enabled, requesting service');
+        loc.requestService();
+      }
+      if (!serviceEnabled) {
+        throw Exception("Cannot enabled location service");
+      }
 
-    PermissionStatus permissionGranted = await loc.hasPermission();
+      PermissionStatus permissionGranted = await loc.hasPermission();
 
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await loc.requestPermission();
-    }
+      if (permissionGranted == PermissionStatus.denied) {
+        permissionGranted = await loc.requestPermission();
+      }
 
-    switch (permissionGranted) {
-      case PermissionStatus.granted:
-      case PermissionStatus.grantedLimited:
-        final LocationData locationData = await loc.getLocation();
-        logger.d('Granted location permission, user location is $locationData');
-        return locationData;
-      case PermissionStatus.denied:
-      case PermissionStatus.deniedForever:
-        logger.e('Permission for location is denied');
-        throw Exception('Location permission is denied');
-    }
+      switch (permissionGranted) {
+        case PermissionStatus.granted:
+        case PermissionStatus.grantedLimited:
+          logger.d('Granted location permission');
+          final LocationData locationData = await loc.getLocation();
+          logger.d('Got result: user location is $locationData');
+          return locationData;
+        case PermissionStatus.denied:
+        case PermissionStatus.deniedForever:
+          logger.e('Permission for location is denied');
+          throw Exception('Location permission is denied');
+      }
+    });
   }
 }
