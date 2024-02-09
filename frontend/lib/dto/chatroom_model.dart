7edf96371df9aea622c3c4e380e5d46e0cbfc351
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-02-09 22:36:56 +0100

    change: changed the chatroom and message structure to suit the realtime database

--- frontend/lib/dto/chatroom_model.dart
+++ frontend/lib/dto/chatroom_model.dart
@@ -6,19 +6,45 @@ import './message_model.dart';
 part 'chatroom_model.g.dart';
 
 @immutable
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class Chatroom {
-  final String initiator;
-  final String receiver;
-  final List<Message> messages;
+  final String roomId;
+  final Participants participants;
+  final Messages messages;
 
   const Chatroom(
-      {required this.initiator,
-      required this.receiver,
+      {required this.roomId,
+      required this.participants,
       required this.messages});
 
   factory Chatroom.fromJson(Map<String, dynamic> json) =>
       _$ChatroomFromJson(json);
 
   Map<String, dynamic> toJson() => _$ChatroomToJson(this);
+}
+
+@JsonSerializable()
+class Participants {
+  final String one;
+  final String two;
+
+  Participants({required this.one, required this.two});
+
+  factory Participants.fromJson(Map<String, dynamic> json) =>
+      _$ParticipantsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ParticipantsToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Messages {
+  final List<Message> one;
+  final List<Message> two;
+
+  Messages({required this.one, required this.two});
+
+  factory Messages.fromJson(Map<String, dynamic> json) =>
+      _$MessagesFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MessagesToJson(this);
 }
