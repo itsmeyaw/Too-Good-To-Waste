c260dbd3e4ea31ec1feea0685e2fcedfc7d01324
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-02-22 22:06:26 +0100

    feat: add google crashlytics

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:logger/logger.dart';
 import 'package:tooGoodToWaste/firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +24,8 @@ Future<void> main() async {
   );
   await PushNotificationsManager().initNotifications();
 
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
  
   runApp(const TooGoodToWaste());
 }
