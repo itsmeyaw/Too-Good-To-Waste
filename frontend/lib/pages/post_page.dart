3a2b0b97fe0a2c71db8b2043655004ab8f60807a
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-02-22 23:01:20 +0100

    feat: add user information in the post page

--- frontend/lib/pages/post_page.dart
+++ frontend/lib/pages/post_page.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:tooGoodToWaste/dto/chatroom_model.dart';
 import 'package:tooGoodToWaste/service/storage_service.dart';
 import 'package:tooGoodToWaste/service/user_service.dart';
 import '../dto/shared_item_model.dart';
@@ -22,117 +22,136 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   final UserService userService = UserService();
   final StorageService storageService = StorageService();
+  final User? currentUser = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text('Shared Item Information'),
           actions: const [],
         ),
-        body: Container(
-            padding: const EdgeInsets.all(10),
-            child:
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              FractionallySizedBox(
-                widthFactor: 1.0,
-                child: SizedBox(
-                    height: 160,
-                    child: GoogleMap(
-                      onMapCreated: (event) {},
-                      initialCameraPosition: CameraPosition(
-                        target: LatLng(widget.postData.location.latitude,
-                            widget.postData.location.longitude),
-                        zoom: 15.0,
-                      ),
-                      circles: <Circle>{
-                        Circle(
-                            circleId: CircleId(widget.postData.id!),
-                            center: LatLng(widget.postData.location.latitude,
-                                widget.postData.location.longitude),
-                            fillColor: const Color.fromRGBO(79, 195, 247, 0.5),
-                            strokeWidth: 1,
-                            strokeColor: const Color.fromRGBO(79, 195, 247, 1),
-                            radius: 200)
-                      },
-                    )),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                widget.postData.name,
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headlineLarge,
-              ),
-              Text(
-                  'Amount: ${widget.postData.amount.nominal} ${widget.postData
-                      .amount.unit}',
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headlineSmall),
-              // TODO: Insert User name here
-              const SizedBox(height: 10,),
-              widget.postData.imageUrl != null ? Center(
-                child: Container(
-                    height: 250,
-                    padding: const EdgeInsets.all(0.0),
-                    child: FutureBuilder(
-                      future: storageService.getImageUrlOfSharedItem(widget.postData.imageUrl!),
-                      builder: (BuildContext context, AsyncSnapshot<String> imageUrlSnapshot) {
-                        if (imageUrlSnapshot.hasData) {
-                          return Image.network(imageUrlSnapshot.requireData, height: 250,);
-                        } else {
-                          return const CircularProgressIndicator();
-                        }
-                      },
-                    )
-                ),
-              ) : const Text('No image provided'),
-              const Spacer(),
-              FutureBuilder(
-                  future: userService.getUserData(widget.postData.user),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<TGTWUser> userSnapshot) {
-                    if (userSnapshot.connectionState == ConnectionState.done &&
-                        userSnapshot.hasData) {
-                      TGTWUser user = userSnapshot.data!;
+        body: FutureBuilder(
+          future: userService.getUserData(widget.postData.user),
+          builder:
+              (BuildContext context, AsyncSnapshot<TGTWUser> userDataSnapshot) {
+            if (userDataSnapshot.connectionState == ConnectionState.done &&
+                userDataSnapshot.hasData) {
+              TGTWUser postUser = userDataSnapshot.requireData;
 
-                      return FilledButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        ChatroomPage(
-                                          secondUserId: widget.postData.user,
-                                          sharedItem: widget.postData,
-                                        )));
-                          },
-                          child: FractionallySizedBox(
-                              widthFactor: 1,
-                              child: Text(
-                                'Chat with ${user.name.last}',
-                                textAlign: TextAlign.center,
-                              )));
-                    } else {
-                      return const FilledButton(
-                          onPressed: null,
-                          child: FractionallySizedBox(
-                              widthFactor: 1,
-                              child: Text(
-                                'Loading',
-                                textAlign: TextAlign.center,
-                              )));
-                    }
-                  }),
-            ])));
+              return Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FractionallySizedBox(
+                          widthFactor: 1.0,
+                          child: SizedBox(
+                              height: 160,
+                              child: GoogleMap(
+                                onMapCreated: (event) {},
+                                initialCameraPosition: CameraPosition(
+                                  target: LatLng(
+                                      widget.postData.location.latitude,
+                                      widget.postData.location.longitude),
+                                  zoom: 15.0,
+                                ),
+                                circles: <Circle>{
+                                  Circle(
+                                      circleId: CircleId(widget.postData.id!),
+                                      center: LatLng(
+                                          widget.postData.location.latitude,
+                                          widget.postData.location.longitude),
+                                      fillColor: const Color.fromRGBO(
+                                          79, 195, 247, 0.5),
+                                      strokeWidth: 1,
+                                      strokeColor:
+                                          const Color.fromRGBO(79, 195, 247, 1),
+                                      radius: 200)
+                                },
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          widget.postData.name,
+                          style: Theme.of(context).textTheme.headlineLarge,
+                        ),
+                        Text(
+                            'Amount: ${widget.postData.amount.nominal} ${widget.postData.amount.unit}',
+                            style: Theme.of(context).textTheme.headlineSmall),
+                        Text(
+                          'Shared by: ${postUser.name.first} ${postUser.name.last}',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        widget.postData.imageUrl != null
+                            ? Center(
+                                child: Container(
+                                    height: 250,
+                                    padding: const EdgeInsets.all(0.0),
+                                    child: FutureBuilder(
+                                      future: storageService
+                                          .getImageUrlOfSharedItem(
+                                              widget.postData.imageUrl!),
+                                      builder: (BuildContext context,
+                                          AsyncSnapshot<String>
+                                              imageUrlSnapshot) {
+                                        if (imageUrlSnapshot.hasData) {
+                                          return Image.network(
+                                            imageUrlSnapshot.requireData,
+                                            height: 250,
+                                          );
+                                        } else {
+                                          return const CircularProgressIndicator();
+                                        }
+                                      },
+                                    )),
+                              )
+                            : const Text('No image provided'),
+                        const Spacer(),
+                        Builder(builder: (BuildContext context) {
+                          if (currentUser != null &&
+                              currentUser!.uid != widget.postData.user) {
+                            return FilledButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ChatroomPage(
+                                                secondUserId:
+                                                    widget.postData.user,
+                                                sharedItem: widget.postData,
+                                              )));
+                                },
+                                child: FractionallySizedBox(
+                                    widthFactor: 1,
+                                    child: Text(
+                                      'Chat with ${postUser.name.last}',
+                                      textAlign: TextAlign.center,
+                                    )));
+                          } else {
+                            return const FilledButton(
+                                onPressed: null,
+                                child: FractionallySizedBox(
+                                    widthFactor: 1,
+                                    child: Text(
+                                      'This is your shared item',
+                                      textAlign: TextAlign.center,
+                                    )));
+                          }
+                        }),
+                      ]));
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 }
