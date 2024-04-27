973ef31d0cbae8c12ecb1894fea43f068a8ec773
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-04-27 22:38:37 +0200

    add: cancel and confirm pick up button

--- frontend/lib/pages/post_page.dart
+++ frontend/lib/pages/post_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:logger/logger.dart';
+import 'package:rive/rive.dart';
 import 'package:tooGoodToWaste/service/shared_items_service.dart';
 import 'package:tooGoodToWaste/service/storage_service.dart';
 import 'package:tooGoodToWaste/service/user_service.dart';
@@ -113,7 +114,7 @@ class _PostPageState extends State<PostPage> {
                             ? Hero(
                         transitionOnUserGestures: true,
                         tag: widget.postData.name,
-                        child: 
+                        child:
                             Center(
                                 child: Container(
                                     height: 250,
@@ -144,7 +145,7 @@ class _PostPageState extends State<PostPage> {
                         Builder(builder: (BuildContext context) {
                           if (currentUser != null &&
                               currentUser!.uid != widget.postData.user) {
-                            return 
+                            return
                              Column(
                               children: <Widget>[
                                 FilledButton.tonal(
@@ -169,27 +170,64 @@ class _PostPageState extends State<PostPage> {
                                 const SizedBox(
                                   height: 10,
                                 ),
-                                FilledButton(
-                                    onPressed: widget.postData.isAvailable && widget.postData.sharedItemReservation == null ? () async {
-                                      try {
-                                        if (await sharedItemService.reserveItem(widget.postData.id!)) {
-                                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Succesfully reserved item")));
-                                        } else {
-                                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cannot reserved item")));
-                                        }
-                                      } catch (e) {
-                                        logger.e("Error when trying to reserve item", error: e);
-                                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There was error while reserving item")));
-                                      }
-                                    } : null,
-                                      child: FractionallySizedBox(
-                                          widthFactor: 1,
-                                          child: Text(
-                                            'Reserve',
-                                            textAlign: TextAlign.center,
+                                Builder(builder: (BuildContext context) {
+                                  if (widget.postData.isAvailable && widget.postData.sharedItemReservation == null) {
+                                    return
+                                      FilledButton(
+                                          onPressed: () async {
+                                            try {
+                                              if (await sharedItemService.reserveItem(widget.postData.id!)) {
+                                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Succesfully reserved item")));
+                                              } else {
+                                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cannot reserved item")));
+                                              }
+                                            } catch (e) {
+                                              logger.e("Error when trying to reserve item", error: e);
+                                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There was error while reserving item")));
+                                            }
+                                          },
+                                          child: const FractionallySizedBox(
+                                              widthFactor: 1,
+                                              child: Text(
+                                                'Reserve',
+                                                textAlign: TextAlign.center,
+                                              )
                                           )
-                                      )
-                                )
+                                      );
+                                  } else if (widget.postData.sharedItemReservation != null && widget.postData.sharedItemReservation!.reserver == currentUser!.uid) {
+                                    // The item is reserved by this user
+                                    return Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                        children: <Widget>[
+                                          IntrinsicWidth(
+                                            child: SizedBox(
+                                              child: FilledButton(
+                                                  style: FilledButton.styleFrom(backgroundColor: Theme.of(context).buttonTheme.colorScheme!.error),
+                                                  onPressed: () {},
+                                                  child: const Text('Cancel')
+                                                  )
+                                            )
+                                          ),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          Expanded(
+                                              child: SizedBox(
+                                                  child: FilledButton (
+                                                      onPressed: () {},
+                                                      child: const Text('Confirm Pick Up')
+                                                  )
+                                              )
+                                          )
+                                        ],
+                                    );
+                                  } else {
+                                    return FilledButton(onPressed: null, child: const FractionallySizedBox(
+                                      widthFactor: 1.0,
+                                      child: Text('Item is already reserved'),
+                                    ));
+                                  }
+                                })
                               ],
 
                              );
